8e34d615dfd4df3ae5f3032a044846d7239f30be
lijiao129311 <977960891@qq.com>
2020-05-24 21:54:01 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,45 @@
+'''
+print("Hello World!")  #字符串
+print(2333)    #整数
+print(2.333)   #小数
+print(True)    #布尔值
+print((1,2))   #元组
+print([1,2])   #数组
+print({1,2})  #字典
+
+
+锄禾日当午 汗滴禾下土 
+谁知盘中餐 粒粒皆辛苦
+
+
+print("呵呵",233,2.333,True)
+print("哈哈"+"嘻嘻")
+print("哈哈"*10)
+print(((1+2)*100-90)/2)
+print(1+1==2)
+
+#变量
+#赋值
+name = "王凯"
+print(name)
+'''
+
+
+
+#数据格式的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = float(input("请输入 ："))
+# b = float(input("请输入 ："))
+# print("input获取的内容: ",a+b)
+
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符的长度是：",len(a) + len(b))
+
